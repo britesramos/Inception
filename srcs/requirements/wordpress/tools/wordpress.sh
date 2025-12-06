361b443c4a07dddaf47bda112010a7011d306633
sramos <sramos@student.codam.nl>
2025-12-06 16:46:28 +0100

    Add another user WP

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -46,6 +46,13 @@ if [ ! -f wp-config.php ]; then
 		--admin_password=${WP_ADMIN_PASSWORD} \
 		--admin_email=${WP_ADMIN_EMAIL} \
 		--allow-root #Bypass restrictions from WP-CLI that prevents commands to run as the root user.(Docker containers often run as root by default.)
+
+	#7)Creating another user
+	echo "Creating additional WordPress user..."
+	wp user create ${WP_USER} ${WP_USER_EMAIL} \
+		--role=author \
+		--user_pass=${WP_USER_PASSWORD} \
+		--allow-root
 fi
 
 #7)Start PHP-FPM (for debian:bookworm fpm8.2 is the version available)
